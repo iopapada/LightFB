328cbe62860a5db817c135ebf358360b4124c36a
John <iopapada@gmail.com>
2015-06-07 13:15:30 +0300

    Complete first page with MVC pattern

---
+++ src/app/controllers/mypage_controller.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John
+ * Date: 7/6/2015
+ * Time: 12:59 μμ
+ */
+
+class MypageController {
+
+    public static function updateInfo()
+    {
+
+    }
+
+    public static function loadFriends()
+    {
+
+    }
+
+    public static function loadMyPhotos()
+    {
+
+    }
+}
