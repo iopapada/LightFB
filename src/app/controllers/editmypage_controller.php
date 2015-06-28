d9a5b44cca44a78a1c16e475520956bffa59dc7e
John <iopapada@gmail.com>
2015-06-28 19:19:45 +0300

    Building my page and edit page...

--- src/app/controllers/editmypage_controller.php
+++ src/app/controllers/editmypage_controller.php
@@ -19,12 +19,12 @@ class EditmypageController {
         $userid = $_SESSION['user_id'];
 
         $profile = $_FILES['avatarfile']['tmp_name'];
+        $blobprofile = "";
         if(!empty($profile)) $blobprofile = mysql_real_escape_string(file_get_contents($profile));
-        else $blobprofile = "";
 
         $cover = $_FILES['coverfile']['tmp_name'];
-        if(!empty($profile)) $blobprofile = mysql_real_escape_string(file_get_contents($cover));
-        else $blobcover = "";
+        $blobcover = "";
+        if(!empty($cover)) $blobcover = mysql_real_escape_string(file_get_contents($cover));
 
         $first = $_POST['Firstname'];
         $last = $_POST['Lastname'];
