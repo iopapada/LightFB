96273884feae781f5de010970d40d58eadb1df42
John <iopapada@gmail.com>
2015-06-30 18:56:26 +0300

    Building my page and edit page...

--- src/app/views/layouts/left_master.html.php
+++ src/app/views/layouts/left_master.html.php
@@ -1,26 +1,65 @@
 <div id="leftbar">
     <ul>
-
-        <h4>My Personal Information</h4>
         <li>
             <a href="index.php?action=myprofile">My Profile</a>
         </li>
         <li>
             <a href="index.php?action=editprofile&name=<?php echo $_SESSION['user_id']?>"  >Edit My Profile</a>
         </li>
-        <h4>My Friends</h4>
-
     </ul>
-<table>
-<tbody>
-<?php
 
-?>
-</tbody>
-</table>
-
+    <div id="phototab">
         <h4>My Photos</h4>
+        <table id="myphoto">
+            <tbody>
+            <?php
+                $userid = $_SESSION['user_id'];
+                $results= db_query_select("SELECT img,imgname FROM images
+                                           INNER JOIN albums ON albums.id = images.albumid
+                                           INNER JOIN userprofile ON userprofile.email = '$userid'");
+
+                for($x = 0; $x<count($results); $x++){
+                    $results[$x]['img'] = base64_encode($results[$x]['img']);
+                }
+                for($i=0;$i<3;$i++) {
+                    echo "<tr>";
+                    for($j=0;$j<3;$j++) {
+                        echo "<td>";
+                        if($j+($i*3) < count($results))echo "<img src=data:image/jpeg;base64," . $results[$j+($i*3)]['img'] . "></img>";
+                        echo "<td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+            </tbody>
+        </table>
+    </div>
 
 
-    </ul>
+    <div id="phototab">
+        <h4>My Friends</h4>
+        <table id="myphoto">
+            <tbody>
+            <?php
+            $userid = $_SESSION['user_id'];
+            $results= db_query_select("SELECT DISTINCT firstname,lastname,pictureURL FROM userprofile
+                                    INNER JOIN friends ON userprofile.email = friends.userid OR userprofile.email = friends.friendid
+                                    WHERE (friends.userid = '$userid' OR friends.friendid = '$userid' ) AND friends.approved ='1'  AND email != '$userid'");
+
+            for($x = 0; $x<count($results); $x++){
+                $results[$x]['pictureURL'] = base64_encode($results[$x]['pictureURL']);
+            }
+            for($i=0;$i<3;$i++) {
+                echo "<tr>";
+                for($j=0;$j<3;$j++) {
+                    echo "<td>";
+                    if($j+($i*3) < count($results))echo "<img src=data:image/jpeg;base64," . $results[$j+($i*3)]['pictureURL'] . "></img>";
+                    echo "<td>";
+                }
+                echo "</tr>";
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
 </div>
--- src/app/views/layouts/left_welcome.html.php
+++ src/app/views/layouts/left_welcome.html.php
@@ -1,6 +1,5 @@
 <div id="left_welcome">
     <ul>
-        <h4>My Personal Information</h4>
         <li>
             <a href="index.php?action=myprofile">My Profile</a>
         </li>
@@ -8,34 +7,42 @@
             <a href="index.php?action=editprofile&name=<?php echo $_SESSION['user_id']?>"  >Edit My Profile</a>
         </li>
 
-        <h4>Groups</h4>
+        <h4>FAVORITES</h4>
         <li>
-
+            <a>New Feed</a>
+            <a>Messages</a>
+            <a>Events</a>
         </li>
 
-        <h4>Apps</h4>
+        <h4>GROUPS</h4>
         <li>
-
+            <a>New Groups</a>
+            <a>Create Group</a>
         </li>
 
-        <h4>Interests</h4>
+        <h4>APPS</h4>
         <li>
-
+            <a>Games</a>
+            <a>Pokes</a>
+            <a>Games Feed</a>
         </li>
 
-        <h4>Events</h4>
+        <h4>INTERESTS</h4>
         <li>
-
+            <a>Pages</a>
         </li>
 
-        <h4>Pages</h4>
+        <h4>EVENTS</h4>
         <li>
-
+            <a>Create Event</a>
         </li>
 
-        <h4>Apps</h4>
+        <h4>PAGES</h4>
         <li>
-
+            <a>Like Pages</a>
+            <a>PAges Feed</a>
+            <a>Create Page</a>
         </li>
+
     </ul>
 </div>
