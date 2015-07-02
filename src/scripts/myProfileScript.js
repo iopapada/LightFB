4e1b37af3ed39d6cadee7e5cb7566e33979ed801
John <iopapada@gmail.com>
2015-07-02 03:09:14 +0300

    Building my page and edit page...

--- src/scripts/myProfileScript.js
+++ src/scripts/myProfileScript.js
@@ -20,6 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('photosBtn').addEventListener('click', loadAllMyPhotos, false);
     }
 
+    document.getElementById('search-query').addEventListener('keypress', function(e){
+        if (e.keyCode == 13) {
+            mainSearch();
+            e.preventDefault();
+        }
+    }, false);
     // Trigger loadMyPosts
     loadMyPosts();
 
